f1e4e909a55ad5783714e514571c7783bff683ee
4logs <4logs>
2020-09-19 16:16:15 +0800

    提交文档

---
+++ IP/info.js
@@ -0,0 +1,58 @@
+if ($response.statusCode != 200) {
+    $done(Null);
+}
+
+var default_regionName = "Oops!!!";
+var default_city = "Oh No!!!";
+var default_org = "Cross Utility Ltd";
+
+/**
+ * @return {string}
+ */
+function RegionName_Check(para) {
+    if (para) {
+        return para;
+    } else {
+        return default_regionName;
+    }
+}
+
+/**
+ * @return {string}
+ */
+function City_Check(para) {
+    if (para) {
+        return para;
+    } else {
+        return default_city;
+    }
+}
+
+/**
+ * @return {string}
+ */
+function Org_Check(para) {
+    if (para) {
+        return para;
+    } else {
+        return default_org;
+    }
+}
+
+var body = $response.body;
+var obj = JSON.parse(body);
+var title = obj['country'];
+var subtitle = default_city + default_org;
+
+var index = obj['org'].indexOf('(');
+if (index != -1) {
+    subtitle = City_Check(obj['city']) + '-' + Org_Check(obj['org'].substring(0, index).trim());
+} else {
+    subtitle = City_Check(obj['city']) + '-' + Org_Check(obj['org']);
+}
+
+var ip = obj['query'];
+//var description = obj['isp'] + '\n' + RegionName_Check(obj['regionName']) + '\n' + obj['query'] + '\n' + obj['timezone'];
+var description = obj['country'] + '\n' + RegionName_Check(obj['regionName']) + '\n' + City_Check(obj['city']) + '\n' + obj['query'] + '\n' + obj['isp'] + '\n' + Org_Check(obj['org']);
+
+$done({title, subtitle, ip, description});
